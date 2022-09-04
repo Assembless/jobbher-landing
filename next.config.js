0427de19fac673a84bdb37030cda2bb662de4d63
norwegianbeginner <r.cichocki.dcm@gmail.com>
2022-09-04 23:52:42 +0200

    :bug: Fix next config

--- next.config.js
+++ next.config.js
@@ -1,28 +1,6 @@
-const withPWA = require('next-pwa');
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
-
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  optimizedImages,
   reactStrictMode: true,
-  cssModules: false,
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
-  },
-  images: {
-    domains: ['source.unsplash.com'],
-  },
 };
 
-module.exports = withPlugins([withPWA, optimizedImages], nextConfig);
+module.exports = nextConfig;
