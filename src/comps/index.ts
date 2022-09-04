33db0085882b1a33268f315916c8b5a822bb0f4e
norwegianbeginner <r.cichocki.dcm@gmail.com>
2022-09-05 00:13:59 +0200

    :bug: Fix deployment

--- src/comps/index.ts
+++
@@ -1,5 +0,0 @@
-export * as Atoms from './atoms';
-export * as Molecules from './molecules';
-export * as Organisms from './organisms';
-export * as Templates from './templates';
-export * as Pages from './pages';
